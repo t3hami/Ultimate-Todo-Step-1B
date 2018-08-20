28d6ef27ac992c8dc4ea640b21728a0c10270f70
Muhammad Tehami <t3hami@gmail.com>
2018-08-21 04:24:50 +0500

    Postgre db ready on heroku.

--- create_db.py
+++ create_db.py
@@ -1,5 +1,26 @@
-from sql_db import db
-from sql_db import Tasks
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
+app = Flask(__name__)
+heroku_uri = 'postgres://ducsshdxzxljof:38f4754e47f7c3e5afec61085d1bbd08e03248d18d50640f80bcd32f3cd43c91@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d9lss7ge2dbpr5'
+app.config['SQLALCHEMY_DATABASE_URI'] = heroku_uri
+db = SQLAlchemy(app)
+
+class Tasks(db.Model):
+    id = db.Column('id', db.Integer, primary_key = True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(200))  
+    done = db.Column(db.Boolean(200))
+
+    def __init__(self, title, description, done):
+        self.title = title
+        self.description = description
+        self.done = done
+
+print('Started.......')
 db.create_all()
 db.session.commit()
+task = Tasks('title','description',False)
+db.session.add(task)
+db.session.commit()
+print('Completed......')
--- sql_db.py
+++ sql_db.py
@@ -4,14 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 heroku_uri = ''
 local_uri = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = local_uri
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ducsshdxzxljof:38f4754e47f7c3e5afec61085d1bbd08e03248d18d50640f80bcd32f3cd43c91@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d9lss7ge2dbpr5'
 db = SQLAlchemy(app)
 
 class SQL:
 
     def __init__(self, app=app):
         local_uri = 'sqlite:///todo.db'
-        app.config['SQLALCHEMY_DATABASE_URI'] = local_uri
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ducsshdxzxljof:38f4754e47f7c3e5afec61085d1bbd08e03248d18d50640f80bcd32f3cd43c91@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d9lss7ge2dbpr5'
         db = SQLAlchemy(app)
 
     def get_all_tasks(self):
