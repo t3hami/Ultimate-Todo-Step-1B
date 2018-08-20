508b0252b8a6e1aca1f03a8dd44c3a64754c7f24
Muhammad Tehami <t3hami@gmail.com>
2018-08-21 02:14:04 +0500

    Made some changes.

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from sql_db import SQL
 
 app = Flask(__name__)
 
-mongodb = SQL(app)
+postgre = SQL(app)
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET', 'POST'])
 def tasks():
     if request.method == 'GET':
-        all_tasks = mongodb.get_all_tasks()
+        all_tasks = postgre.get_all_tasks()
         return jsonify(all_tasks)
 
     elif request.method == 'POST':
@@ -19,7 +19,7 @@ def tasks():
                 'description': request.json['description'],
                 'done': False
             }
-            id = mongodb.post_task(task)
+            id = postgre.post_task(task)
             return jsonify({'status': 'Success', '_id': str(id)})
         except KeyError:
             return jsonify({'status': 'Missing title or discription, or both.'})
@@ -28,18 +28,18 @@ def tasks():
 @app.route('/todo/api/v1.0/tasks/<task_id>', methods=['GET', 'PUT', 'DELETE'])
 def task(task_id):
     if request.method == 'GET':
-        if mongodb.is_task_present(task_id):
-            task = mongodb.get_task(task_id)
+        if postgre.is_task_present(task_id):
+            task = postgre.get_task(task_id)
             return jsonify(task)
         else:
             return jsonify({'status': 'There is no task with _id: ' + task_id})
 
     elif request.method == 'PUT':
-        if mongodb.is_task_present(task_id):
+        if postgre.is_task_present(task_id):
             if not request.json:
                 return jsonify({'status': 'Please provide atleast one key, value to update'})
             old_task = {}
-            old_task = mongodb.get_task(task_id)
+            old_task = postgre.get_task(task_id)
             old_task['_id'] = int(old_task['_id'])
             updated_task = {}
             for key in old_task.keys():
@@ -47,14 +47,14 @@ def task(task_id):
                     updated_task[key] = request.json[key]
                 except:
                     updated_task[key] = old_task[key]
-            mongodb.update_task(task_id, updated_task)
+            postgre.update_task(task_id, updated_task)
             return jsonify({'status': 'Success'})
         else:
             return jsonify({'status': 'There is no task with _id: ' + task_id})
 
     elif request.method == 'DELETE':
-        if mongodb.is_task_present(task_id):
-            mongodb.delete_task(task_id)
+        if postgre.is_task_present(task_id):
+            postgre.delete_task(task_id)
             return jsonify({'status': 'Success'})
         else:
             return jsonify({'status': 'There is no task with _id: ' + task_id})
